2a7a3a700f45ea2ad98dae34b3e5863aee93119d
AuXy666 <awais.shahid_143@yahoo.com>
2022-12-25 02:55:23 +0500

    Register Activity Added with FrontEnd

--- app/src/main/java/com/example/eventtra/Registration.java
+++ app/src/main/java/com/example/eventtra/Registration.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Registration extends AppCompatActivity {
-
+    //To be Implemented
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
