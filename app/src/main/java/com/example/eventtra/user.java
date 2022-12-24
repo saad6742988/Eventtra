d8e948bd636b81fc4f6a50c412fa377c322fed55
AuXy666 <awais.shahid_143@yahoo.com>
2022-12-25 02:53:59 +0500

    Register Activity Added with FrontEnd

---
+++ app/src/main/java/com/example/eventtra/user.java
@@ -0,0 +1,60 @@
+package com.example.eventtra;
+
+import com.google.firebase.firestore.Exclude;
+
+public class user {
+    private String fname;
+    private String lname;
+    private String email;
+    private String phone;
+    private String userId;
+
+    public user(String fname, String lname, String email, String phone) {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    @Exclude
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
