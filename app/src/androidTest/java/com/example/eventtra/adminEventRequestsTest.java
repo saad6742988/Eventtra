3f250135f34aa6fe3aa4df0a084b4e116c55ba9d
AuXy666 <awais.shahid_143@yahoo.com>
2023-03-31 08:37:08 +0500

    modifying test cases 1.0

--- app/src/androidTest/java/com/example/eventtra/adminEventRequestsTest.java
+++ app/src/androidTest/java/com/example/eventtra/adminEventRequestsTest.java
@@ -13,8 +13,10 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 
 public class adminEventRequestsTest {
+
+    //ID:00005
     @Test
-    public void populateListValid()
+    public void dataValid1()
     {
 
 
@@ -27,10 +29,37 @@ public class adminEventRequestsTest {
 
 
     }
-
+    //ID:00006
     @Test
-    public void populateListInvalid() {
+    public void dataValid2()
+    {
+
+
+        EventRequestModel test=new EventRequestModel("2", "Daira2","This is Daira 2", "Accept" , "12");
+
+        eventRequestStub testGetter=new eventRequestStub();
+        EventRequestModel test2= testGetter.getList(2);
+        Log.d("test 2", ""+test2);
+        Assert.assertEquals(test,test2);
+
+
+    }
+
+    //ID:00007
+    @Test
+    public void dataInvalid1() {
         EventRequestModel test=new EventRequestModel("1", "Daira2","This is Daira", "Reject" , "11");
+
+        eventRequestStub testGetter=new eventRequestStub();
+        EventRequestModel test2= testGetter.getList(1);
+        Log.d("test 2", ""+test2);
+        Assert.assertNotEquals(test,test2);
+
+    }
+    //ID:00008
+    @Test
+    public void dataInvalid2() {
+        EventRequestModel test=new EventRequestModel("1", "Daira3","This is Daira", "Reject" , "11");
 
         eventRequestStub testGetter=new eventRequestStub();
         EventRequestModel test2= testGetter.getList(1);
--- app/src/androidTest/java/com/example/eventtra/subEventRequestsTest.java
+++ app/src/androidTest/java/com/example/eventtra/subEventRequestsTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 public class subEventRequestsTest {
 
     @Test
-    public void subEventRequestsValid()
+    public void subEventRequestsValid1()
     {
         String uritemp="https://www.example.com/path/to/resource?query=value";
         Uri uri2=Uri.parse(uritemp);
@@ -24,12 +24,43 @@ public class subEventRequestsTest {
 
     }
     @Test
-    public void subEventRequestsNotValid()
+    public void subEventRequestsValid2()
+    {
+        String uritemp="https://www.example.com/path/to/resource?query=value";
+        Uri uri2=Uri.parse(uritemp);
+
+        subEventsModel test=new subEventsModel("2","Cricket","This is Cricket","100","Awais", uri2,"1","2/3/2023","16:16:16",5,true);
+        subEventsStub testGetter= new subEventsStub();
+
+        subEventsModel test2= testGetter.getList(2);
+        Assert.assertEquals(test,test2);
+
+
+    }
+
+    @Test
+    public void subEventRequestsNotValid1()
     {
 
         String uritemp="https://www.example.com/path/to/resource?query=value";
         Uri uri2=Uri.parse(uritemp);
         subEventsModel test=new subEventsModel("1","Dota 3","This is not released","100","Awais", uri2,"1","2/3/2023","16:16:16",5,false);
+        subEventsStub testGetter= new subEventsStub();
+
+        subEventsModel test2= testGetter.getList(1);
+        Assert.assertNotEquals(test,test2);
+
+
+    }
+
+
+    @Test
+    public void subEventRequestsNotValid2()
+    {
+
+        String uritemp="https://www.example.com/path/to/resource?query=value";
+        Uri uri2=Uri.parse(uritemp);
+        subEventsModel test=new subEventsModel("1","Tekken 7","This is not released","100","Awais", uri2,"1","2/3/2023","16:16:16",5,false);
         subEventsStub testGetter= new subEventsStub();
 
         subEventsModel test2= testGetter.getList(1);
